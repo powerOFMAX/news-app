6fba34159e644cc5cfa399872778cff4134e4640
powerOFMAX <max_maxi@live.com.ar>
2020-08-13 23:15:56 -0300

    Lint issues fixed / Articles from local fixed

--- utils/index.js
+++ utils/index.js
@@ -1,39 +1,7 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-useless-catch */
 import axios from 'axios';
-import * as LocalData from './Endpoint';
-
-export const getArticles = async () => {
-  try {
-    const url = process.env.REACT_APP_ENDPOINT_URL || '';
-    if (!url) return LocalData.articles;
-    const response = await axios.get(url);
-    const filteredResults = response.data.articles.filter(
-      (element) => element.subtype === '7',
-    );
-    const sortedTags = sortTags(filteredResults);
-    return { results: filteredResults, tags: sortedTags };
-  } catch (error) {
-    throw error;
-  }
-};
-
-const sortTags = (tags) => {
-  let tagsCollection = [];
-  const counter = {};
-  // Iterate through the Tags and merge all the objects in one
-  tags.forEach((element) => {
-    tagsCollection = [
-      ...merge(element.taxonomy.tags, tagsCollection, 'slug', counter),
-    ];
-  });
-
-  // Sort the elements in DESC order | Example ["frutas-tid67217", [4, "Frutas"]]
-  const entries = Object.entries(counter);
-  const sorted = entries.sort((a, b) => b[1][0] - a[1][0]);
-
-  // Once we sorted the elements we transform them into an Object and we pick the top 10
-  const top10 = sorted.map((item) => [item[0], item[1][1]]).slice(0, 10);
-  return top10;
-};
+import * as LocalData from './Endpoint.json';
 
 /**
  * - Merge A & B values
@@ -61,3 +29,38 @@ function merge(a, b, prop, counter) {
   );
   return reduced.concat(b);
 }
+
+const sortTags = (tags) => {
+  let tagsCollection = [];
+  const counter = {};
+  // Iterate through the Tags and merge all the objects in one
+  tags.forEach((element) => {
+    tagsCollection = [
+      ...merge(element.taxonomy.tags, tagsCollection, 'slug', counter),
+    ];
+  });
+
+  // Sort the elements in DESC order | Example ["frutas-tid67217", [4, "Frutas"]]
+  const entries = Object.entries(counter);
+  const sorted = entries.sort((a, b) => b[1][0] - a[1][0]);
+
+  // Once we sorted the elements we transform them into an Object and we pick the top 10
+  const top10 = sorted.map((item) => [item[0], item[1][1]]).slice(0, 10);
+  return top10;
+};
+
+export const getArticles = async () => {
+  try {
+    const url = process.env.REACT_APP_ENDPOINT_URL || '';
+    const response = url ? await axios.get(url) : LocalData.default;
+    const filteredResults = response.data.articles.filter(
+      (element) => element.subtype === '7',
+    );
+    const sortedTags = sortTags(filteredResults);
+    return { results: filteredResults, tags: sortedTags };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default getArticles;
